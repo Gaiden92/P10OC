bc9e5404e8c04f7419ad3a286758311e0846435c
Gaiden92 <samy_fouchal@hotmail.fr>
2023-12-08 23:48:01 +0100

    initialisation projet + installation poetry

---
+++ api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth import base_user
+
+class User(base_user.AbstractBaseUser):
+    pass
+
+class Project():
+    pass
+
+class Comment():
+    pass
